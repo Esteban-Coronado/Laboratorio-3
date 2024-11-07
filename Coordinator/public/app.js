999fe94eff2c02b5d5824308741e3801ee8e72d5
Andres <andresmaldonado200338@gmail.com>
2024-11-06 23:07:14 -0500

    Changes coordinator visibility

--- Coordinator/public/app.js
+++ Coordinator/public/app.js
@@ -14,12 +14,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const response = await fetch('/instances');
     const instances = await response.json();
     instanceList.innerHTML = '';
+    
     instances.forEach(({ host, port }) => {
       const item = document.createElement('li');
-      item.textContent = `Host: ${host}, Port: ${port}`;
+      
+      const link = document.createElement('a');
+      link.href = `http://${host}:${port}`;
+      link.textContent = `Host: ${host}, Port: ${port}`;
+      link.target = '_blank';
+      link.title = 'Ir a la instancia';
+  
+      item.appendChild(link);
       instanceList.appendChild(item);
     });
-  }
+  }  
 
   async function launchInstance() {
     const response = await fetch('/launch', { method: 'POST' });
